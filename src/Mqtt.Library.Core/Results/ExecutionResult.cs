ab0d6e3d7d54530f1f9a06090316878d368fe8f1
Orest Hurskyi <orest.hurskiy@sius.com>
2022-05-26 00:09:50 +0300

    add IExecutionResult

---
+++ src/Mqtt.Library.Core/Results/ExecutionResult.cs
@@ -0,0 +1,20 @@
+﻿namespace Mqtt.Library.Core.Results
+{
+    public class ExecutionResult : IExecutionResult
+    {
+        protected ExecutionResult() { }
+
+        private ExecutionResult(string failureReason)
+        {
+            FailureReason = failureReason;
+        }
+
+        public string FailureReason { get; } = string.Empty;
+        
+        public bool Success  => string.IsNullOrEmpty(FailureReason);
+
+        public static ExecutionResult Ok() => new ExecutionResult();
+
+        public static ExecutionResult Fail(string reason) => new ExecutionResult(reason);
+    }
+}
---
+++ src/Mqtt.Library.Core/Results/IExecutionResult.cs
@@ -0,0 +1,8 @@
+﻿namespace Mqtt.Library.Core.Results
+{
+    public interface IExecutionResult
+    { 
+        bool Success { get; }
+        string FailureReason { get; }
+    }
+}
