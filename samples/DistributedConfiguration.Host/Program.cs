c01e831f65981f5440da535a122abeec8ce7e282
Orest Hurskyi <orest.hurskiy@sius.com>
2022-05-26 18:49:19 +0300

    wip

--- samples/DistributedConfiguration.Host/Program.cs
+++ samples/DistributedConfiguration.Host/Program.cs
@@ -1,5 +1,6 @@
 using Mqtt.Library.Client.Local;
 using MqttLibrary.Examples.Pairing.Domain;
+using Serilog;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((hostContext, serviceCollection) =>
@@ -13,6 +14,10 @@ IHost host = Host.CreateDefaultBuilder(args)
         serviceCollection.AddPairingDomainServices();
 
     })
+    .UseSerilog((hostingContext, _, loggerConfiguration) => loggerConfiguration
+        .ReadFrom.Configuration(hostingContext.Configuration)
+        .Enrich.FromLogContext()
+        .WriteTo.Console())
     .Build();
 
 await host.RunAsync();
