2c0c1ab0f5b4e1ea495b1427737a47bd30441add
Orest Hurskyi <orest.hurskiy@sius.com>
2022-06-30 19:11:11 +0300

    wip

--- src/MessagingLibrary.Core/ServiceCollectionExtensions.cs
+++ src/MessagingLibrary.Core/ServiceCollectionExtensions.cs
@@ -1,5 +1,7 @@
-﻿using MessagingLibrary.Core.Configuration;
+﻿using System.Reflection;
+using MessagingLibrary.Core.Configuration;
 using MessagingLibrary.Core.Factory;
+using MessagingLibrary.Core.Handlers;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
@@ -12,6 +14,21 @@ public static class ServiceCollectionExtensions
     {
         serviceCollection.TryAddTransient<HandlerFactory>(p => p.GetRequiredService);
         serviceCollection.TryAddSingleton<IMessageHandlerFactory<TMessagingClientOptions>, MessageHandlerFactory<TMessagingClientOptions>>();
+        return serviceCollection;
+    }
+    
+    public static IServiceCollection AddMessageHandlers(this IServiceCollection serviceCollection, Assembly[] assemblies)
+    {
+        var implementationTypes = assemblies
+            .SelectMany(a => a.DefinedTypes)
+            .Where(t => typeof(IMessageHandler).IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract)
+            .ToList();
+
+        foreach (var handlerType in implementationTypes)
+        {
+            serviceCollection.AddTransient(handlerType);
+        }
+
         return serviceCollection;
     }
 }
--- src/MessagingLibrary.Processing/ServiceCollectionExtensions.cs
+++ src/MessagingLibrary.Processing/ServiceCollectionExtensions.cs
@@ -2,7 +2,6 @@
 using MessagingLibrary.Core;
 using MessagingLibrary.Core.Configuration;
 using MessagingLibrary.Core.Factory;
-using MessagingLibrary.Core.Handlers;
 using MessagingLibrary.Processing.Executor;
 using MessagingLibrary.Processing.Strategy;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,24 +14,11 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddMessagingPipeline<TMessagingClientOptions>(this IServiceCollection serviceCollection, params Assembly[] assemblies)
         where TMessagingClientOptions: IMessagingClientOptions
     {
-        ConnectMessageHandlers(serviceCollection, assemblies);
+        serviceCollection.AddMessageHandlers(assemblies);
 
         serviceCollection.AddRequiredServices<TMessagingClientOptions>();
 
         return serviceCollection;
-    }
-
-    private static void ConnectMessageHandlers(IServiceCollection serviceCollection, Assembly[] assemblies)
-    {
-        var implementationTypes = assemblies
-            .SelectMany(a => a.DefinedTypes)
-            .Where(t => typeof(IMessageHandler).IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract)
-            .ToList();
-
-        foreach (var handlerType in implementationTypes)
-        {
-            serviceCollection.AddTransient(handlerType);
-        }
     }
 
     private static IServiceCollection AddRequiredServices<TMessagingClientOptions>(this IServiceCollection serviceCollection) 
