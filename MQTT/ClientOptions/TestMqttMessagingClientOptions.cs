d4f7079c56e098cf3e90b0f44c736e8c7f649648
Orest Hurskyi <orest.hurskiy@sius.com>
2022-07-01 14:01:34 +0300

    remove old

--- MQTT/ClientOptions/TestMqttMessagingClientOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿using Mqtt.Library.Client.Configuration;
-
-namespace Mqtt.Library.Test.ClientOptions;
-
-public class TestMqttMessagingClientOptions : IMqttMessagingClientOptions
-{
-    public MqttBrokerConnectionOptions MqttBrokerConnectionOptions { get; set; } =  new() { Host = "localhost", Port = 1883 };
-}
