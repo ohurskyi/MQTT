dd9b2d21d30e1b35de2ceac02dde3928e6e48723
Orest Hurskyi <orest.hurskiy@sius.com>
2022-07-10 18:27:32 +0300

    wip

--- nuget pack core.ps1
+++ nuget pack core.ps1
@@ -1,5 +1,7 @@
 $coreProj = ".\src\MessagingLibrary\MessagingLibrary.Core\MessagingLibrary.Core.csproj"
 $artifacts = ".\artifacts"
+$version = "1.0.1-alpha"
+
 dotnet clean $coreProj -c Release
 dotnet build $coreProj -c Release
-dotnet pack $coreProj -c Release -o $artifacts --no-build
+dotnet pack $coreProj -c Release -p:packageVersion=$version -o $artifacts --no-build
