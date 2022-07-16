c3f68e89cc0e82b3b2f0a02fa42cdf9a611f38d0
Orest Hurskyi <orest.hurskiy@sius.com>
2022-07-16 16:34:23 +0300

    Delete nuget pack core.ps1

--- nuget pack core.ps1
+++
@@ -1,7 +0,0 @@
-$coreProj = ".\src\MessagingLibrary\MessagingLibrary.Core\MessagingLibrary.Core.csproj"
-$artifacts = ".\artifacts"
-$version = "1.0.1-alpha"
-
-dotnet clean $coreProj -c Release
-dotnet build $coreProj -c Release
-dotnet pack $coreProj -c Release -p:packageVersion=$version -o $artifacts --no-build
