e13e209696031e180083a628f7dd23083bdc403b
Orest Hurskyi <ohurskiy@gmail.com>
2022-07-22 20:04:01 +0300

    Update IMessageHandlerFactory.cs

--- src/MessagingLibrary/MessagingLibrary.Core/Factory/IMessageHandlerFactory.cs
+++ src/MessagingLibrary/MessagingLibrary.Core/Factory/IMessageHandlerFactory.cs
@@ -3,17 +3,11 @@ using MessagingLibrary.Core.Handlers;
 
 namespace MessagingLibrary.Core.Factory;
 
-public interface IMessageHandlerFactory
+public interface IMessageHandlerFactory<TMessagingClientOptions> where TMessagingClientOptions: IMessagingClientOptions
 {
     int RegisterHandler(Type handlerType, string topic);
     int RegisterHandler<THandler>(string topic) where THandler : class, IMessageHandler;
     int RemoveHandler<THandler>(string topic) where THandler : class, IMessageHandler;
     int RemoveHandler(Type handlerType, string topic);
     IEnumerable<IMessageHandler> GetHandlers(string topic, ServiceFactory serviceFactory);
-}
-
-public interface IMessageHandlerFactory<TMessagingClientOptions> : IMessageHandlerFactory
-    where TMessagingClientOptions: IMessagingClientOptions
-{
-    
 }
